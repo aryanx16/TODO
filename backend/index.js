7b8ea7e6773a4c5258328de86eb19da7beab7220
Aryan Babare <babreab17@gmail.com>
2024-10-12 12:37:04 +0530

    handling cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ const jwt = require("jsonwebtoken")
 const { Todos } = require("./db")
 const { User } = require("./db")
 const cors = require("cors")
-app.use(cors())
+const corsOptions = {
+    origin: "http://localhost:5173", // Frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // If you are using cookies or authorization headers
+};
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions)); // This allows preflight requests
 app.use(express.json())
 
 
@@ -30,6 +36,10 @@ const authMiddleware =(req, res, next) => {
 app.get("/",(req,res)=>{
     console.log("server is running")
     res.send("server is running ")
+})
+app.get("/gett",(req,res)=>{
+    console.log("getttin.....")
+    res.send("getttin.....")
 })
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
