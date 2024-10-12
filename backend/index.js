d302287c154b08a9e98d31eefa2fa946a0d4fd01
Aryan Babare <babreab17@gmail.com>
2024-10-12 16:47:52 +0530

    k

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,9 @@ const { Todos } = require("./db")
 const { User } = require("./db")
 const cors = require("cors")
 const app = express()
-const corsOptions = {
-    origin: "*", // Frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // If you are using cookies or authorization headers
-};
-app.use(cors(corsOptions))
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(204); // No content
-});// This allows preflight requests
+app.use(cors({
+    origin:"*",
+}))
 app.use(express.json())
 
 
